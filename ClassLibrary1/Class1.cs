ff9ec786d9f9fcf4d333ff57b8e98c733cff55b9
Ryadn <ryadnov-97@yandex.ru>
2023-01-13 10:37:31 +0300

    Добавить реализацию репозитория книг в памяти

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Store
+namespace ClassLibrary1
 {
-    public interface IBookRepository
+    public class Class1
     {
-        Book[] GetAllByTitle (string titlePart);
     }
 }
