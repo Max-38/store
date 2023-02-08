2e1b2d9030f421d056ec0fd6b9c9c861f63f069d
Ryadn <ryadnov-97@yandex.ru>
2023-02-08 04:23:51 +0300

    Сделать код асинхронным

--- plugins/Store.YandexKassa/Areas/YandexKassa/Models/ExampleModel.cs
+++ plugins/Store.YandexKassa/Areas/YandexKassa/Models/ExampleModel.cs
@@ -9,6 +9,6 @@ namespace Store.YandexKassa.Areas.YandexKassa.Models
     public class ExampleModel
     {
         public int OrderId { get; set; }
-        public string ReturnUri { get; set; }
+        public string? ReturnUri { get; set; }
     }
 }
--- presentation/Store.Web/Program.cs
+++ presentation/Store.Web/Program.cs
@@ -37,8 +37,8 @@ builder.Services.AddSingleton<OrderService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-if (true)
+if (!app.Environment.IsDevelopment())
+//if (true)
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
